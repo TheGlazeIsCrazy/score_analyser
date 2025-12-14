f6eaddd55e5fd2262075cfc2e07481386bc70f73
TheGlazeIsCrazy <shayaan.mehdi@yahoo.com>
2025-12-14 13:07:59 +0000

    Created analyser.py
    The analyser.py has been created to include skeletons of the five key functions: parse_scores(path), throw_exception(scores, pass_threshold), calculate_average(scores), convert_to_letters(scores) and analyse_scores(scores), as well as the if __name__ == "__main__" block.

---
+++ analyser.py
@@ -0,0 +1,18 @@
+def parse_scores(path: str) -> list:
+    pass
+
+def throw_exception(scores: list, pass_threshold: float) -> str:
+    pass
+
+def convert_to_letters(scores: list) -> list:
+    pass
+
+def calculate_average(scores: list) -> float:
+    pass
+
+def analyse_scores(scores: list) -> float:
+    pass
+
+if __name__ == "__main__":
+    path = input("Please enter a file path: ")
+    pass_threshold = float(input("Please enter a pass threshold: "))
