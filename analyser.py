400f2631251cfea8912827a0d0044bbd1114a3e0
TheGlazeIsCrazy <shayaan.mehdi@yahoo.com>
2025-12-14 13:41:21 +0000

    Updated analyser.py
    The throw_exception(scores, pass_threshold) function has been completed to return the error message for the first noticed error: invalid file path, invalid pass threshold, non-numeric scores, invalid scores, no scores. The parse_scores(path) function has been revised in this process.

--- analyser.py
+++ analyser.py
@@ -1,9 +1,26 @@
 def parse_scores(path: str) -> list:
     with open(path) as data:
-        return data.read().split(",")
+        try:
+            return data.read().split(",")
+        except:
+            return "invalid path"
 
 def throw_exception(scores: list, pass_threshold: float) -> str:
-    pass
+    if scores == "invalid path":
+        return "Error. The provided file path does not exist."
+    
+    if not(0 <= pass_threshold <= 100):
+        return "Error. The pass threshold is not between zero and 100."
+
+    for score in scores:
+        if type(score) != float:
+            return "Error. The provided file contains non-numeric scores. Ensure that the list is comma-separated."
+        
+        if not(0 <= score <= 100):
+            return "Error. The provided file contains scores not between 0 and 100."
+        
+    if scores == [""]:
+        return "Error. The provided file is empty."
 
 def convert_to_letters(scores: list) -> list:
     pass
