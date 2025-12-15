715f22149f43500ea9426f3c28b2e0492fa39100
TheGlazeIsCrazy <shayaan.mehdi@yahoo.com>
2025-12-15 18:17:00 +0000

    Polished analyser.py
    The analyser.py has been polished to implement a letter grade distribution, returning the number of scores of each letter grade.

--- analyser.py
+++ analyser.py
@@ -22,22 +22,28 @@ def throw_exception(scores: list, pass_threshold: float) -> str:
         if not(0 <= score <= 100):
             return "Error. The provided file contains scores not between 0 and 100."
 
-def convert_to_letters(scores: list) -> list: # This returns an arbitrarily long list of strings.
+def convert_to_letters(scores: list) -> list: # This returns two arbitrarily long lists of strings.
     letters = []
+    counts = [0, 0, 0, 0, 0] # The first element is the number of "A"s, the second is the number of "B"s, etc.
 
     for score in scores: # This loop is based on a common US percentage scale.
         if score >= 90:
             letters.append("A")
+            counts[0] += 1
         elif score >= 80:
             letters.append("B")
+            counts[1] += 1
         elif score >= 70:
             letters.append("C")
+            counts[2] += 1
         elif score >= 60:
             letters.append("D")
+            counts[3] += 1
         else:
             letters.append("F")
+            counts[4] += 1
 
-    return letters
+    return letters, counts
 
 def calculate_average(scores: list) -> float:
     return sum(scores) / len(scores)
@@ -50,17 +56,23 @@ def analyse_scores(scores: list) -> str: # Most of the statistics are calculated
             pass_count += 1
 
     return f"""
+    A: {counts[0]},
+    B: {counts[1]},
+    C: {counts[2]},
+    D: {counts[3]},
+    F: {counts[4]}.
+
     Scores: {",".join(parsed_scores)}
-    Average: {average}, {convert_to_letters([average])[0]}
-    Highest: {max(scores)}, {convert_to_letters([max(scores)])[0]}
-    Lowest: {min(scores)}, {convert_to_letters([min(scores)])[0]}
-    Pass count: {pass_count}
+    Average: {average}, {convert_to_letters([average])[0][0]}
+    Highest: {max(scores)}, {convert_to_letters([max(scores)])[0][0]}
+    Lowest: {min(scores)}, {convert_to_letters([min(scores)])[0][0]}
+    Pass count: {pass_count}.
     """
 
     # Line 54 puts the variable average inside a list in order to satisfy the convert_to_letters(scores) function.
-    # The above function returns a list of strings, which has a length of 1, in this case.
-    # To format this as a string instead of a 1-element list, for aesthetic purposes, [0] is used to return a string.
-    # Therefore, the full formatting is: f"Average: {average}, {convert_to_letters([average])[0]}"
+    # The above function returns two lists of strings, which each have a length of 1, in this case.
+    # To format this as a string instead of a 1-element list, for aesthetic purposes, [0][0] is used to return a string.
+    # Therefore, the full formatting is: f"Average: {average}, {convert_to_letters([average])[0][0]}"
 
     # A similar principle is used on lines 55 and 56.
 
@@ -87,7 +99,7 @@ if __name__ == "__main__":
     if exception:
         print(exception)
     else:
-        letters = convert_to_letters(scores)
+        letters, counts = convert_to_letters(scores)
         average = calculate_average(scores)
 
         print(analyse_scores(scores))
