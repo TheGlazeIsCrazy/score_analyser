14212b89fee7e6686c848eedb8b9b8ed57201cb8
TheGlazeIsCrazy <shayaan.mehdi@yahoo.com>
2025-12-15 17:54:46 +0000

    Updated analyser.py
    The if __name__ == "__main__" block has been completed to obtain the file path and the pass threshold, cleanse the file path, analyse the list of scores and output basic statistics regarding the list. During the process, various other functions have underwent minor debugging.

--- analyser.py
+++ analyser.py
@@ -1,26 +1,26 @@
 def parse_scores(path: str) -> list:
-    with open(path) as data:
-        try:
+    try:
+        with open(path) as data:
             return data.read().split(",")
-        except:
-            return "invalid path"
+    except:
+        return "invalid path"
 
 def throw_exception(scores: list, pass_threshold: float) -> str:
     if scores == "invalid path":
         return "Error. The provided file path does not exist."
+        
+    if scores == [""]:
+        return "Error. The provided file is empty."
     
     if not(0 <= pass_threshold <= 100):
         return "Error. The pass threshold is not between zero and 100."
 
     for score in scores:
         if type(score) != float:
-            return "Error. The provided file contains non-numeric scores. Ensure that the list is comma-separated."
+            return "Error. The provided file may contain non-numeric scores. If this is not the case, ensure that the list is comma-separated."
         
         if not(0 <= score <= 100):
             return "Error. The provided file contains scores not between 0 and 100."
-        
-    if scores == [""]:
-        return "Error. The provided file is empty."
 
 def convert_to_letters(scores: list) -> list:
     letters = []
@@ -50,7 +50,7 @@ def analyse_scores(scores: list) -> str:
             pass_count += 1
 
     return f"""
-    Scores: {",".join(scores)}
+    Scores: {",".join(parsed_scores)}
     Average: {average}, {convert_to_letters([average])[0]}
     Highest: {max(scores)}, {convert_to_letters([max(scores)])[0]}
     Lowest: {min(scores)}, {convert_to_letters([min(scores)])[0]}
@@ -61,4 +61,25 @@ if __name__ == "__main__":
     path = input("Please enter a file path: ")
     pass_threshold = float(input("Please enter a pass threshold: "))
 
-    average = None
+    parsed_scores = parse_scores(path)
+
+    scores = []
+
+    if parsed_scores == "invalid path":
+        scores = "invalid path"
+    else:
+        for to_parse in parsed_scores:
+            try:
+                scores.append(float(to_parse))
+            except:
+                scores.append(to_parse)
+
+    exception = throw_exception(scores, pass_threshold)
+
+    if exception:
+        print(exception)
+    else:
+        letters = convert_to_letters(scores)
+        average = calculate_average(scores)
+
+        print(analyse_scores(scores))
